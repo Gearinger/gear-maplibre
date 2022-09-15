8afaf557f8026df8500f4c989563cb26a93c7fea
Gearinger <50980099+Gearinger@users.noreply.github.com>
2022-09-15 10:20:24 +0800

    add glyphs

--- src/common/MaplibreUtil.ts
+++ src/common/MaplibreUtil.ts
@@ -1,14 +1,146 @@
-import { MapGeoJSONFeature,Map,Marker } from "maplibre-gl"
-import * as maplibregl from "maplibre-gl"
-import * as turf from '@turf/turf'
+import { center, flattenReduce, Point } from '@turf/turf'
+import { Map, Marker, LngLat, Popup } from "maplibre-gl"
+import { geojson } from "flatgeobuf";
 
-export function drawAnno(feature: MapGeoJSONFeature, map: Map) {
-    let pos = turf.center(feature.toJSON());
+/**
+ * 将 FlatGeoBuf 数据添加到地图上
+ * @param fgb FlatGeobuf
+ * @param map 地图
+ */
+export async function addFlatGeoBuf(fgb: any, map: Map) {
+    const fc = { type: "FeatureCollection", features: [] as any[] };
+    let i = 0;
 
-    var marker = new Marker()
-        .setLngLat([30.5, 50.5])
-        .addTo(map);
+    for await (const f of geojson.deserialize(
+        fgb,
+        undefined,
+        undefined
+    ) as AsyncGenerator<any>) {
+        fc.features.push({ ...f, id: i });
+        i += 1;
+    }
 
-    
+    map.addSource("counties", {
+        type: "geojson",
+        data: fc,
+    });
+    map.addLayer({
+        id: "counties-fill",
+        type: "fill",
+        source: "counties",
+        paint: {
+            "fill-color": "#0000FF",
+            "fill-opacity": [
+                "case",
+                ["boolean", ["feature-state", "hover"], false],
+                1,
+                0.5,
+            ],
+        },
+    });
 }
 
+/**
+ * 添加geojson数据到地图上
+ */
+export function addTestGeoJson(map: Map) {
+    map.addSource("testGeoJson", {
+        type: "geojson",
+        // data: "http://192.168.10.95:8999/geoserver/nansha/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=nansha%3Afield&maxFeatures=40000&outputFormat=application%2Fjson",
+        data: "./data/smallGeoJSON.geojson"
+    });
+    map.addLayer({
+        id: "testGeoJson",
+        type: "fill",
+        source: "testGeoJson",
+        layout: {},
+        paint: {
+            "fill-color": "#088",
+            "fill-opacity": 0.8,
+            "fill-outline-color": "red",
+        },
+    });
+    map.addLayer({
+        id: "testGeoJsonAnno",
+        type: "symbol",
+        source: "testGeoJson",
+        layout: {
+            "text-field": "{name}"
+        },
+    });
+
+    map.fitBounds(map.getBounds());
+}
+
+/**
+ * 添加pbf数据到地图上
+ */
+export function addPbfLayer(map: Map) {
+    map.addSource("pbfLayer", {
+        type: "vector",
+        tiles: ["http://127.0.0.1:9005/business/field/pbfLayer/field/1/{z}/{x}/{y}"],
+    });
+    map.addLayer({
+        id: "pbfLayer",
+        type: "fill",
+        source: "pbfLayer",
+        "source-layer": "default",
+        "paint": {
+            "fill-color": [
+                "match",
+                ["get", "name"],
+                ["0403020000"],
+                "#CDEBF2",
+                ["0206000000"],
+                "#F8CDD0",
+                ["0201000000"],
+                "#92D050",
+                "#F39F72",
+            ]
+        }
+    });
+    map.fitBounds(map.getBounds());
+    // console.log(map);
+}
+
+
+/**
+ * 添加标注
+ * @param feature 要素
+ * @param map 地图
+ * @returns 
+ */
+export function drawAnno(feature: maplibregl.MapGeoJSONFeature, map: maplibregl.Map): maplibregl.Marker {
+    let pos = center(feature.toJSON()).geometry as Point;
+
+    let htmlEle = new HTMLElement();
+    htmlEle.innerHTML = pos.coordinates[0] + "," + pos.coordinates[1];
+    var marker = new Marker({ element: htmlEle })
+        .setLngLat(new LngLat(pos.coordinates[0], pos.coordinates[1]))
+        .addTo(map);
+    return marker;
+}
+
+/**
+ * 标记当前所在位置
+ * @param map 
+ */
+export function markCurrentPos(map: maplibregl.Map) {
+    navigator.geolocation.getCurrentPosition(e => {
+
+        let htmlEle = document.createElement('button');
+        htmlEle.innerHTML = e.coords.longitude + "," + e.coords.latitude;
+        var marker = new Marker({ element: htmlEle })
+            .setLngLat(new LngLat(e.coords.longitude, e.coords.latitude))
+            .addTo(map);
+        // var marker = new Marker({ color: "#ff0000" })
+        //     // 设置标记位置
+        //     .setLngLat(new LngLat(e.coords.longitude, e.coords.latitude))
+        //     // 添加弹出框
+        //     .setPopup(new Popup().setHTML(e.coords.longitude + "_" + e.coords.latitude + "_" + e.coords.altitude))
+        //     .addTo(map);
+        // // console.log("当前定位显示了吗？？？");
+    }, () => {
+        alert("定位失败！");
+    });
+}
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,113 +4,54 @@
 
 <script setup lang="ts">
 import { onMounted } from "vue";
-import maplibregl, { Feature, Map, StyleSpecification } from "maplibre-gl";
+import { Feature, Map, StyleSpecification, NavigationControl, GeoJSONFeature, MapLayerEventType } from "maplibre-gl";
+import * as maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import MapboxDraw from "mapbox-gl-draw";
 import "mapbox-gl-draw/dist/mapbox-gl-draw.css";
-import { geojson } from "flatgeobuf";
 
-/**
- * 添加geojson数据到地图上
- */
-function addTestGeoJson(map: Map) {
-  map.addSource("testGeoJson", {
-    type: "geojson",
-    data: "http://192.168.10.95:8999/geoserver/nansha/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=nansha%3Afield&maxFeatures=40000&outputFormat=application%2Fjson",
-  });
-  map.addLayer({
-    id: "testGeoJson",
-    type: "fill",
-    source: "testGeoJson",
-    layout: {},
-    paint: {
-      "fill-color": "#088",
-      "fill-opacity": 0.8,
-      "fill-outline-color": "red",
-    },
-  });
-
-  map.fitBounds(map.getBounds());
-  //   map.fitBounds(layer.getBounds());
-}
-
-/**
- * 添加pbf数据到地图上
- */
-function addPbfLayer(map: Map) {
-  map.addSource("pbfLayer", {
-    type: "vector",
-    tiles: ["http://127.0.0.1:9005/business/field/pbfLayer/field/{z}/{x}/{y}"],
-  });
-  map.addLayer({
-    id: "pbfLayer",
-    type: "fill",
-    source: "pbfLayer",
-    "source-layer": "default",
-    "paint": {
-      "fill-color": [
-        "match",
-        ["get", "name"],
-        ["0403020000"],
-        "#CDEBF2",
-        ["0206000000"],
-        "#F8CDD0",
-        ["0201000000"],
-        "#92D050",
-        "#F39F72",
-      ]
-    }
-  });
-
-  map.fitBounds(map.getBounds());
-}
-
-/**
- * 将 FlatGeoBuf 数据添加到地图上
- * @param fgb FlatGeobuf
- * @param map 地图
- */
-async function addFlatGeoBuf(fgb: any, map: Map) {
-  const fc = { type: "FeatureCollection", features: [] as any[] };
-  let i = 0;
-
-  for await (const f of geojson.deserialize(
-    fgb,
-    undefined,
-    undefined
-  ) as AsyncGenerator<any>) {
-    fc.features.push({ ...f, id: i });
-    i += 1;
-  }
-
-  map.addSource("counties", {
-    type: "geojson",
-    data: fc,
-  });
-  map.addLayer({
-    id: "counties-fill",
-    type: "fill",
-    source: "counties",
-    paint: {
-      "fill-color": "#0000FF",
-      "fill-opacity": [
-        "case",
-        ["boolean", ["feature-state", "hover"], false],
-        1,
-        0.5,
-      ],
-    },
-  });
-  map.querySourceFeatures("")
-}
+import { drawAnno, markCurrentPos, addFlatGeoBuf, addTestGeoJson } from "../common/MaplibreUtil"
+import * as mapUtil from "map-gl-utils"
 
 // 挂载时初始化地图
 onMounted(() => {
+  const map = initMap();
+  addControllers(map);
+
+  // 地图加载时，添加 geojson 数据
+  map.on("load", async () => {
+    addTestGeoJson(map);
+    // addPbfLayer(map);
+    // const response = await fetch("./data/fgb.fgb").then((res) =>
+    //   addFlatGeoBuf(res.body, map)
+    // );
+
+    markCurrentPos(map);
+
+    console.log(map);
+
+    // map.setLayoutProperty('testGeoJson', 'text-color', "#ff0000");
+  });
+
+  // 点击地图时，获取点击位置的要素
+  map.on("click", (e) => {
+    console.log(e);
+    let feas = map.queryRenderedFeatures(e.point, {});
+    console.log(feas);
+  });
+
+});
+
+/**
+ * 初始化地图
+ */
+function initMap(): Map {
   // 创建空白图层样式，用于地图初始化
   const blankStyle: StyleSpecification = {
     version: 8,
     name: "BlankMap",
     sources: {},
+    glyphs:"./data/glyphs/{fontstack}/{range}.pbf",
     layers: [
       {
         id: "background",
@@ -123,38 +64,28 @@ onMounted(() => {
     ],
   };
   // 初始化地图
-  var map = new maplibregl.Map({
+  var map = new Map({
     container: "map", // container id
     // style: "https://demotiles.maplibre.org/style.json", // style URL
-    style:
-      "https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
-    // style: blankStyle,
+    // style: "https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
+    style: blankStyle,
     center: [113.46, 22.88], // starting position [lng, lat]
     zoom: 9, // starting zoom
   });
+  return map;
+}
 
+/**
+ * 添加控件
+ * @param map 
+ */
+function addControllers(map: Map) {
   // 添加常用地图控件
-  map.addControl(new maplibregl.NavigationControl({}));
-
+  map.addControl(new NavigationControl({}));
   // 添加地图绘制控件
-  map.addControl(new MapboxDraw(), "top-left");
-
-  // 地图加载时，添加 geojson 数据
-  map.on("load", async () => {
-    // addTestGeoJson(map);
-    addPbfLayer(map);
-    // const response = await fetch("./data/fgb.fgb").then((res) =>
-    //   addFlatGeoBuf(res.body, map)
-    // );
-  });
-
-  // 点击地图时，获取点击位置的要素
-  map.on("click", (e) => {
-    console.log(e);
-    let feas = map.queryRenderedFeatures(e.point, {});
-    console.log(feas);
-  });
-});
+  const draw = new MapboxDraw();
+  map.addControl(draw, "top-left");
+}
 </script>
 
 <style>
